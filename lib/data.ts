61c1c8ab0f416666ac2ba34ea92c9b8b473c8cd8
Akhil <akhil1659@gmail.com>
2025-12-09 14:56:53 +0530

    intro and work section completed

---
+++ lib/data.ts
@@ -0,0 +1,15 @@
+export const jobs = [
+    {
+        duration: "Feb 2023 – Aug 2023",
+        role: "Full Stack Developer Intern",
+        company: "Tech Helps",
+        description: [
+            "Developed JavaScript-based dynamic parser, automating backend script creation and reducing developmenttime by 40%.",
+            "Managed real-estate website backend, optimizing data flow, and collaborating with frontend team for seamless property listing and sales.",
+            "Implemented RESTful APIs, integrated external services, and conducted rigorous testing, resulting in a 20% decrease in reported bugs for enhanced website functionality and stability."
+        ],
+        tech: ["Next.js", "TypeScript", "NestJS", "PostgreSQL"],
+    },
+    
+    
+]
