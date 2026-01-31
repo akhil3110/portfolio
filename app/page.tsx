bd97bb646a97573fcd1c24f28defd2c7e088de05
Akhil <akhil1659@gmail.com>
2026-02-01 00:59:49 +0530

    changes in ui (reoved snow effect)

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,10 @@ import HomeClient from "@/components/home/HomeClient";
 import JobItem from "@/components/home/JobItem";
 import ProjectsList from "@/components/home/ProjectsList";
 import { jobs, projects, socials } from "@/lib/data";
-import Image from "next/image";
-import Snowfall from "react-snowfall";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground relative ">
-      <Snowfall  snowflakeCount={200}/>
       <HomeClient />
 
       <main className="max-w-5xl mx-auto px-6 sm:px-8 lg:px-16">
